ee8a9bfcf88f1c5f9fdd5c5afb73fce23ddffc88
Mateusz Martin <mateusz.martinn@gmail.com>
2022-08-12 22:26:55 +0200

    Initial commit

---
+++ public/js/stopwatch.js
@@ -0,0 +1,3 @@
+let hours = document.getElementsByClassName('stopwatch__timer--hours')
+let minutes = document.getElementsByClassName('stopwatch__timer--minutes')
+let seconds = document.getElementByClassName ('')
